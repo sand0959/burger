e4ad735fa1b348898b679a248225c38b60fb9681
ted sandridge <sand0959@gmail.com>
2017-05-27 16:22:55 -0400

    testing to see if this fixes anything

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+	connection = mysql.createConnection({
 	host: process.env.db_host || "localhost",
 	user: process.env.db_user || "root",
-	password: process.env.db_pw || "br22y79nn77",
-	port: 3306,
+	password: process.env.db_pw || "",
 	database: process.env.db || 'burgers_db'
 });
-
+};
 connection.connect(function(err) {
 	if (err) {
 		console.error('Error connecting: ' + error.stack);
